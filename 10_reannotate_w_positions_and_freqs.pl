4c7ef3a4e5d75a6abcc6c50ea1051f8792e13e1f
Ivana <ivana.mihalek@gmail.com>
2016-09-21 16:36:33 -0400

    multiple vcfs

--- 10_reannotate_w_positions_and_freqs.pl
+++ 10_reannotate_w_positions_and_freqs.pl
@@ -6,12 +6,13 @@ use variant_utils_pl::jannovar qw(jannovar);
 use variant_utils_pl::vcfanno  qw(vcfanno);
 
 @ARGV ||
-    die "Usage:  $0  <file name> \n";
-my $filename = $ARGV[0];
+    die "Usage:  $0  <file name> [<file name 2> ...] \n";
+my @filenames = @ARGV;
 
+foreach my $filename ( @filenames) {
+    my $stripped_filename  = strip_vcf($filename);
+    my $jannovar_filename  = jannovar ($stripped_filename);
+    my $annotated_filename = vcfanno  ($jannovar_filename);
 
-my $stripped_filename  = strip_vcf($filename);
-my $jannovar_filename  = jannovar ($stripped_filename);
-my $annotated_filename = vcfanno  ($jannovar_filename);
-
-printf "\nfinal annotated file: $annotated_filename\n\n";
+    printf "\nfinal annotated file: $annotated_filename\n\n";
+}

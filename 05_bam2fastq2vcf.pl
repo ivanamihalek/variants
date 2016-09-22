ac0d639df63575f054898939bb0baeecee7c67a1
Ivana <ivana.mihalek@gmail.com>
2016-09-22 15:05:23 -0400

    bam2fastq2vcf

---
+++ 05_bam2fastq2vcf.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+@ARGV>1 ||
+    die "Usage:  $0  <file name> \n";
+my @filename = @ARGV[0];
+
+############
+$samtools  = "/home/ivana/third/SeqMule/exe/samtools/samtools";
+$bam2fastq = "/home/ivana/third/bedtools2/bin/bamToFastq";
+$seqmule   = "/home/ivana/third/SeqMule/bin/seqmule";
+
+foreach ($samtools, $bam2fastq, $seqmule) {
+    -e $_ || die "$_ not found\n";
+}

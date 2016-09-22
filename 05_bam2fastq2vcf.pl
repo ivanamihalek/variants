62eb1e50c8120c18887b9bbb3622d33859ed24dc
Ivana <ivana.mihalek@gmail.com>
2016-09-22 15:38:47 -0400

    bam2fastq2vcf

--- 05_bam2fastq2vcf.pl
+++ 05_bam2fastq2vcf.pl
@@ -3,9 +3,9 @@
 @ARGV ||
     die "Usage:  $0  <file name> \n";
 my $bamfile = $ARGV[0];
-my $qsort = $bamfile;
-$qsort =~ s/\.bam$/.qsort/;
-die "$bamfile  $qsort\n";
+my $qsort_root = $bamfile;
+$qsort_root =~ s/\.bam$/.qsort/;
+my $qsort_file = $qsort_root.".bam";
 
 ############
 $samtools  = "/home/ivana/third/SeqMule/exe/samtools/samtools";
@@ -16,4 +16,10 @@ foreach ($samtools, $bam2fastq, $seqmule) {
     -e $_ || die "$_ not found\n";
 }
 
-$cmd = "$samtools sort -n ";
+if (-e $qsort_file and ! -z $qsort_file ) {
+    print "$qsort_file found.\n"
+} else {
+    $cmd = "$samtools sort -n $bamfile $qsort_root";
+    print "running $cmd ...\n";
+    (system $cmd) && die "error: $!\n";
+}

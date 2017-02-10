06c93584f7934e8ea2367e1a0f552e8b93b1ad14
ivana <ivana.mihalek@gmail.com>
2017-02-09 19:17:33 -0500

    find missing variants

--- 02_find_cases_with_missing_variants_on_bronto.py
+++ 02_find_cases_with_missing_variants_on_bronto.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
 import sys
-from os import listdir
+import  os
 import  subprocess
 
 from  variant_utils_py.generic_utils import *
 from  variant_utils_py.mysqldb import *
 
 ####################################
-def main():
-
+def find_boids_without_variants():
+    boids = []
     db     = connect_to_mysql()
     cursor = db.cursor()
     switch_to_db (cursor, 'blimps_production')
@@ -17,11 +17,30 @@ def main():
     qry  = 'select i.boid from individuals i left join variants v '
     qry += 'on i.id=v.individual_id where v.individual_id is null'
     rows  = search_db (cursor, qry)
-    for row in rows:
-        print row[0]
-
+    boids = [row[0] for row in rows]
     cursor.close()
     db.close()
+    return boids
+
+####################################
+def main():
+    boids = find_boids_without_variants()
+    if len(boids) == 0:
+        print "All boids seem to have variants today."
+        return
+    for boid in boids:
+        # find directory
+        paths = []
+        for toplevel_dir in ["/data01", "data02"]:
+            paths += [path for path, dir, file in os.walk(toplevel_dir) if boid in dir]
+        if len(path) == 0:
+            print "No data found for %s" % boid
+            continue
+        if len(path) > 1:
+            print "Data  for %s found in several places. THis might require some attention" % boid
+            print "\n".join(paths)
+            continue
+        print boid, path
     return
 
 

c6bbd58c18788341858b835ab46f16c3eee4bc25
ivana <ivana.mihalek@gmail.com>
2017-02-12 18:10:49 -0500

    consensus vcf

--- 06_realignment_pipe_outer_loop.pl
+++ 06_realignment_pipe_outer_loop.pl
@@ -4,21 +4,23 @@ use warnings FATAL => 'all';
 
 #infile is just alist of boids
 @ARGV==3 || die "Usage: $0  <infile> <workdir> <year>.\n";
-my ($infile, $orkdir, $year) = @ARGV;
+my ($infile, $workdir, $year) = @ARGV;
 my $realn = "/home/ivana/pypeworks/variants/05_realignment_pipe.pl";
 
-for ($infile, $orkdir, $realn) {
+for ($infile, $workdir, $realn) {
     (-e $_) || die "$_ not found.\n";
 }
-$orkdir =~ "current" || die "workir has no current in its path - sure you want to use it?\n";
+$workdir =~ "current" || die "workir has no current in its path - sure you want to use it?\n";
+chdir $workdir;
+
 my $yr = substr $year, 2, 2;
 my @boids = split "\n", `grep BO$yr $infile`;
 for my $boid (@boids) {
     print $boid, "\n";
     # parse boid into year/case/individual (year I have already)
     # run 05_relignment pipe year c
-    my $case       = substring $boid, 4, 3;
-    my $individual = substring $boid, 7, 2;
+    my $case       = substr $boid, 4, 3;
+    my $individual = substr $boid, 7, 2;
     `$realn $year $case $individual`;
     `rm -f BO* seqmule*`;
 }

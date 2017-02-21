20d54e950762876a50366bef0473a6794ca5cb91
ivana <ivana.mihalek@gmail.com>
2017-02-21 15:07:08 -0500

    annotate all

--- 10_reannotate_w_positions_and_freqs.pl
+++ 10_reannotate_w_positions_and_freqs.pl
@@ -1,12 +1,38 @@
 #!/usr/bin/perl -w
 use strict;
 use lib  '/home/ivana/pypeworks/variants';
+use warnings FATAL => 'all';
 use variant_utils_pl::generic qw(strip_vcf);
 use variant_utils_pl::annovar qw(annovar);
 use variant_utils_pl::vcfanno  qw(vcfanno);
 
-@ARGV==1 || die "Usage:  $0  <filename file>  \n";
-my @filenames = split "\n", `cat  $ARGV[0]`;
+(@ARGV==1) || die "Usage:  $0  <filename file>/all  \n";
+
+my @filenames = ();
+if ($ARGV eq "all") {
+    foreach my $dir ("/data01", "/data02") {
+        my @allfiles = split "\n", `find $dir  -name "*consensus.vcf"`;
+        foreach my $file (@allfiles) {
+            my $annotated = $file;
+            $annotated =~ s/vcf$/annotated.vcf/;
+            if ( -e $annotated) {
+                printf "$annotated found \n";
+            } else {
+                printf "adding $annotated\n";
+                push @filenames, $annotated;
+            }
+        }
+    }
+} else {
+    @filenames = split "\n", `cat  $ARGV[0]`;
+}
+
+
+for my $filename ( @filenames) {
+    print " ***** $filename \n";
+}
+
+exit;
 
 foreach my $filename ( @filenames) {
     my $stripped_filename  = strip_vcf($filename);

238b80269bd25c97ef9f41bfab2962cd51a6cb82
ivana <ivana.mihalek@gmail.com>
2017-03-08 14:51:59 -0500

    redo seqmule variants

--- 08_variants_from_dropbox.py
+++ 08_variants_from_dropbox.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python -w
 
-# here, in distinction to 05_realignemnt_pipe, we start from fastq files,
+# here, in distinction to 05_realignemnt_pipe, we start from bam (seqmule's own)  files,
 # downloaded from Dropbox - that's why it has to be python
 
 
@@ -25,7 +25,7 @@ def main():
 	topdir = "/raw_data"
 	year = "20"+boid[2:4]
 	caseno = boid[4:7]
-	dropbox_folder = "/".join([topdir, year, caseno, boid,"wes"])
+	dropbox_folder = "/".join([topdir, year, caseno, boid,"wes/alignments/by_seqmule_pipeline"])
 	print dropbox_folder
 	return True
 

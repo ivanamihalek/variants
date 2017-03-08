47bcb74d4c0c807c853efad4d447c8bede483381
ivana <ivana.mihalek@gmail.com>
2017-03-08 14:48:44 -0500

    redo seqmule

---
+++ 08_variants_from_dropbox.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python -w
+
+# here, in distinction to 05_realignemnt_pipe, we start from fastq files,
+# downloaded from Dropbox - that's why it has to be python
+
+
+from  variant_utils_py.generic_utils import *
+from  variant_utils_py.dropbox_utils import *
+import commands
+
+
+####################################
+DROPBOX_TOKEN = os.environ['DROPBOX_TOKEN']
+
+dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+
+####################################
+def main():
+
+	if len(sys.argv) < 2:
+		print  "usage: %s <BOid>" % sys.argv[0]
+		exit(1)
+	boid =	sys.argv[1]
+
+	topdir = "/raw_data"
+	year = "20"+boid[2:4]
+	caseno = boid[4:7]
+	dropbox_folder = "/".join([topdir, year, caseno, boid,"wes"])
+	print dropbox_folder
+	return True
+
+
+
+####################################
+if __name__ == '__main__':
+	main()
+

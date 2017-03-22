c1355c2ad9398735e437e82278c36454845decfa
ivana <ivana.mihalek@gmail.com>
2017-03-22 19:40:16 -0400

    bam existence check

--- 06_realignment_pipe.pl
+++ 06_realignment_pipe.pl
@@ -72,8 +72,6 @@ if ($bamfile =~ /.bam$/) {
     exit (0);
 }
 
-exit;
-
 ##########################################
 # check if the parts of the pipeline have already completed
 my $logfile = "$boid.script";

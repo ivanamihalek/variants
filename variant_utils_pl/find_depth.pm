78b96147a796c681d8dd76f5b0dd7d782e187136
ivana <ivana.mihalek@gmail.com>
2017-05-17 14:25:40 -0400

    phred

--- variant_utils_pl/find_depth.pm
+++ variant_utils_pl/find_depth.pm
@@ -51,6 +51,7 @@ sub find_depth (@) {
             }
             # find the same position in other vcf files in the same folder
             my ($chrom, $pos) = @aux[0..1];
+            ($pos == 120404629) && print " *****************  afdgkfahgla \n";
             my ($ref, $alt) = @aux[3..4];
             my $depth_found = check_depth_field_exists_in_other_files ($chrom, $pos, $ref, $alt, \@alt_vcf_files);
             if ( ! $depth_found) {
